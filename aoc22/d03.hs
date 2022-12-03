eafaa2170277312e339d13bf832b3c1d28beb40e
worldOneo <62426504+worldOneo@users.noreply.github.com>
2022-12-03 16:05:40 +0100

    2022 day 3 - small version

--- aoc22/d03.hs
+++ aoc22/d03.hs
@@ -3,8 +3,9 @@
 module Main (main) where
 
 import Data.Char
+import Data.List
 import Data.List.Split qualified as Split
-import Data.Set (fromList, member)
+import Data.Set (fromList, member, splitAt)
 import Debug.Trace
 import Utils qualified as U
 
@@ -13,6 +14,10 @@ main = do
   input <- filter (/= U.pack "") . U.lines <$> U.readFile "03.txt"
   print $ d3p1 input
   print $ d3p2 input
+  r <- d3p1'
+  print r
+  r2 <- d3p2'
+  print r2
 
 score :: Char -> Int
 score c
@@ -39,3 +44,17 @@ d3p2 =
           . U.triplet
       )
     . Split.chunksOf 3
+
+d3p1' :: IO Int
+d3p1' =
+  sum
+    . map (score . head . uncurry intersect . U.halfList)
+    . lines
+    <$> readFile "03.txt"
+
+d3p2' :: IO Int
+d3p2' =
+  sum
+    . map (score . head . foldl1 intersect)
+    . Split.chunksOf 3 . lines
+    <$> readFile "03.txt"
