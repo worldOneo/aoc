261e58bd9b394e119d13d0cb27f775faf866e545
worldOneo <62426504+worldOneo@users.noreply.github.com>
2022-12-07 09:46:00 +0100

    2022 day 7 - case pattern matching

--- aoc22/d07.hs
+++ aoc22/d07.hs
@@ -1,11 +1,9 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
-import Data.Char
 import Data.List (intercalate, sortBy)
 import Data.List.Split
 import Data.Map as M (Map, adjust, elems, insert, lookup, singleton)
 import Data.Maybe
-import Utils qualified as U
 
 main :: IO ()
 main = do
@@ -15,23 +13,18 @@ main = do
   let empty = 70000000 - (sum . fromMaybe undefined . M.lookup "") fs
   print $ (minimum . filter (>= (30000000 - empty)) . map sum . M.elems) fs
 
-fill :: [String] -> Int -> Map String [Int] -> Map String [Int]
-fill [] _ m = m
-fill xs s m = fill (init xs) s (M.adjust (s :) (intercalate "/" xs) m)
+addToParents :: [String] -> Int -> Map String [Int] -> Map String [Int]
+addToParents [] _ m = m
+addToParents xs s m = addToParents (init xs) s (M.adjust (s :) (intercalate "/" xs) m)
 
 parse :: [String] -> [String] -> Map String [Int] -> Map String [Int]
 parse [] _ dirs = dirs
 parse (cmd : ins) cur dirs = do
   let d = splitOn " " cmd
-  if head d == "$"
-    then
-      if d !! 1 == "cd"
-        then
-          if d !! 2 == ".."
-            then parse ins (init cur) dirs
-            else parse ins (cur ++ [d !! 2]) dirs
-        else parse ins cur dirs
-    else
-      if isDigit (head (head d))
-        then parse ins cur (fill cur (head (U.ints (head d))) dirs)
-        else parse ins cur (M.insert (intercalate "/" (cur ++ [d !! 1])) [] dirs)
+  case d of
+    ["$", "cd", ".."] -> parse ins (init cur) dirs
+    ["$", "cd", n] -> parse ins (cur ++ [n]) dirs
+    ("$" : _) -> parse ins cur dirs
+    ["dir", f] -> parse ins cur (M.insert (intercalate "/" (cur ++ [f])) [] dirs)
+    (s : _) -> parse ins cur (addToParents cur (head (read s)) dirs)
+    _ -> undefined
