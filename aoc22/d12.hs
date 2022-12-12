2eb861d6e68048166322a820257d54d46cc045e6
worldOneo <62426504+worldOneo@users.noreply.github.com>
2022-12-12 15:56:02 +0100

    2022 day 12 - a->E optimization

--- aoc22/d12.hs
+++ aoc22/d12.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -O2 #-}
-
 import Data.Array
 import Data.Char (ord)
 import Data.List (elemIndex)
@@ -14,8 +12,8 @@ main = do
   let info = constructMap input
   let (tx, ty) = find 0 'S' (lines input)
   let (gx, gy) = find 0 'E' (lines input)
-  print $ hunt (tx, ty) (gx, gy) info
-  print $ (minimum . map (\start -> hunt start (gx, gy) info)) (findAll (0, -1) "" (lines input) [])
+  print $ hunt (tx, ty) (< 2) 'E' info
+  print $ hunt (gx, gy) (> (-2)) 'a' info
   where
     find :: Int -> Char -> [String] -> (Int, Int)
     find _ _ [] = undefined
@@ -23,16 +21,11 @@ main = do
       let x = fromMaybe (-1) (elemIndex c s)
       if x /= -1 then (x, i) else find (i + 1) c ss
 
-    findAll :: (Int, Int) -> String -> [String] -> [(Int, Int)] -> [(Int, Int)]
-    findAll _ _ [] r = r
-    findAll (_, y) [] (s : ss) r = findAll (0, y + 1) s ss r
-    findAll (x, y) (c : s) ss r = findAll (x + 1, y) s ss (if c == 'a' then (x, y) : r else r)
-
 constructMap :: String -> Input
 constructMap s = (listArray (0, length (lines s)) . map (\x -> listArray (0, length x) x) . lines) s
 
-hunt :: (Int, Int) -> (Int, Int) -> Input -> Int
-hunt (tx, ty) (gx, gy) inp = run S.empty [(tx, ty, 0)]
+hunt :: (Int, Int) -> (Int -> Bool) -> Char -> Input -> Int
+hunt (tx, ty) isAllowed goal inp = run S.empty [(tx, ty, 0)]
   where
     reachable (x, y) = do
       let real o
@@ -41,14 +34,13 @@ hunt (tx, ty) (gx, gy) inp = run S.empty [(tx, ty, 0)]
             | otherwise = ord o
       let current = real ((inp ! y) ! x)
       let row = inp ! y
-      let nu = [(x, y + 1) | y < (length inp - 2) && (real ((inp ! (y + 1)) ! x) - current) < 2]
-      let nd = if y > 0 && abs (real ((inp ! (y - 1)) ! x) - current) < 2 then (x, y - 1) : nu else nu
-      let nr = if x < (length row - 2) && (real (row ! (x + 1)) - current) < 2 then (x + 1, y) : nd else nd
-      let nl = if x > 0 && (real (row ! (x - 1)) - current) < 2 then (x - 1, y) : nr else nr
-      nl
+      let nu = [(x, y + 1) | y < (length inp - 2) && isAllowed (real ((inp ! (y + 1)) ! x) - current)]
+      let nd = if y > 0 && isAllowed (real ((inp ! (y - 1)) ! x) - current) then (x, y - 1) : nu else nu
+      let nr = if x < (length row - 2) && isAllowed (real (row ! (x + 1)) - current) then (x + 1, y) : nd else nd
+      if x > 0 && isAllowed (real (row ! (x - 1)) - current) then (x - 1, y) : nr else nr
 
     run _ [] = 99999999999999
     run visited ((x, y, l) : steps)
       | S.member (x, y) visited = run visited steps
-      | x == gx && y == gy = l
+      | ((inp ! y) ! x) == goal = l
       | otherwise = run (S.insert (x, y) visited) (steps ++ map (\(nx, ny) -> (nx, ny, l + 1)) (reachable (x, y)))
